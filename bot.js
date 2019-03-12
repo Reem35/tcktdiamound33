0229756b49f8e27b979ef0b884a2f445c9099cf1
Reem35 <43762012+Reem35@users.noreply.github.com>
2019-03-12 15:39:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 //Ticket System
 client.on("message", (message) => {
   
-   if (message.content.startsWith(".new-ticket")) {     
+   if (message.content.startsWith(".new")) {     
         const reason = message.content.split(" ").slice(1).join(" ");     
         if (!message.guild.roles.exists("name", "Ticket Team")) return message.channel.send(`This server doesn't have a \`Ticket Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Ticket Team");
+        message.guild.createChannel(`ticket-${message.author.username}`, "ask").then(c => {
+            let role = message.guild.roles.find("name", "Support Team ");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
@@ -33,7 +33,7 @@ client.on("message", (message) => {
  
  
   if (message.content.startsWith(".close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+        if (!message.channel.name.startsWith(`ticket`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`.confirm\`. This will time out in 10 seconds and be cancelled.`)
            .then((m) => {
